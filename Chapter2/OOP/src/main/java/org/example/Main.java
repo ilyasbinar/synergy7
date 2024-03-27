ad36bd4ecd63b032e748aa842297411555a77d04
ilyas <ilyas@lua.co.id>
2024-03-27 17:06:00 +0700

    hapus sout

--- Chapter2/OOP/src/main/java/org/example/Main.java
+++ Chapter2/OOP/src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import org.example.controller.ProductController;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test1");
 //        CollectionTutorial.set();
         Data.initiateProduct();
         ProductController pc = new ProductController();
