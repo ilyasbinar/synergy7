3709aa74abb84926407981fddaa4118a7b73ec08
ilyas <ilyas@lua.co.id>
2024-03-27 17:00:12 +0700

    tambah sout di main

--- Chapter2/OOP/src/main/java/org/example/Main.java
+++ Chapter2/OOP/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.example.controller.ProductController;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test1");
 //        CollectionTutorial.set();
         Data.initiateProduct();
         ProductController pc = new ProductController();
