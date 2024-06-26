3ba72c26f77b293f431ab743e34422c6b66c4184
birruby <milyas@binaracademy.org>
2024-06-26 21:02:00 +0700

    docker

---
+++ Chapter8/docker-demo/src/main/java/com/example/doker_demo/DokerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.doker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokerDemoApplication.class, args);
+	}
+
+}
---
+++ Chapter8/docker-demo/src/test/java/com/example/doker_demo/DokerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DokerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
